ec80ce48b314ae3db36185dff27468e474e1e4a4
Daryan <daryan.sankar1@gmail.com>
2017-08-17 15:12:24 -0400

    default bot depth changed from 8 to 7

--- src/connect/Main.java
+++ src/connect/Main.java
@@ -14,7 +14,7 @@ public class Main {
 					null, JOptionPane.PLAIN_MESSAGE));
 			
 			Board board = new Board(height, width, connect);
-			Bot bot = new Bot(Board.BLACK, 8);
+			Bot bot = new Bot(Board.BLACK, 7);
 			Game game = new Game(board, bot);
 			new GUI(game);
 			game.start();
